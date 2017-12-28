4d996e097d89b76a210a1fffd0d60c8fb953c324
Simon 徐 <simon@sc.10086.cn.defaultbadlist>
2017-12-28 23:07:32 +0800

    to test wanchehui

--- src/core/accesses.js
+++ src/core/accesses.js
@@ -1,10 +1,41 @@
-function checkModulePressionByRoleCollection(roleCollection, moduleString){
-  return false;
+import { Roles } from '/imports/api/roles.js';
+
+function checkModulePressionByRoleCollection(roleCollection, moduleString, opera){
+  let isAccess = false
+  for (var i = 0; i < roleCollection.length; i++) {
+      let role = Roles.find({name: roleCollection[i]});
+      if (role[moduleString]!=undefined) {
+        isAccess =  role[moduleString][opera];
+        continue;
+      }else{
+        continue;
+      }
+  }
+  return isAccess;
 }
 function checkACLByRoleCollection(roleCollection, ACL){
-  return false;
+  let matchCount = 0;
+  for (var i = 0; i < ACL.length; i++) {
+
+    for (var j= 0; j < roleCollection.length; j++) {
+      if (roleCollection[j]==ACL[i]) {
+        matchCount++;
+        break;
+      }
+    }
+  }
+  if (matchCount != ACL.length) {
+    return false
+  }
+  return true;
 }
-export function canBeAccessed(roleCollection, moduleString, ACL, weight){
+export function canBeAccessed(
+  roleCollection,
+   moduleString,
+   ACL,
+   weight,
+   opera //read edit buy delete
+ ){
   //若是权重没有没有达标，不管如何吹牛逼，就是通不过。
   if (ACL.weight < weight) {
     return "ACCESS DENY"
@@ -12,14 +43,12 @@ export function canBeAccessed(roleCollection, moduleString, ACL, weight){
   if (roleCollection.weight < weight) {
     return "WEIGHT ACCESS DENY";
   }
-  if (!checkModulePressionByRoleCollection(roleCollection, moduleString)) {
-    return "ROLE ACCESS DENY";
-  }
   if (!checkACLByRoleCollection(roleCollection, ACL)) {
     return "ACL ACCESS DENY"
   }
-
-
+  if (!checkModulePressionByRoleCollection(roleCollection, moduleString)) {
+    return "ROLE ACCESS DENY";
+  }
 
   return true;
 }
---
+++ testTempFiles/acl.js
@@ -0,0 +1,27 @@
+const ACL={
+  read: {
+    roles: [nobody,showOner],
+    users: [],
+    timeRange: {
+      begin:
+      end:
+    }
+  },
+  write: {
+    roles: [],
+    users: [],
+    timeRange: {
+      begin:
+      end:
+    }
+  },
+  buy: {
+    roles: [],
+    users: [],
+    timeRange: {
+      begin: new Date(),
+      end:
+    }
+
+  }
+}
